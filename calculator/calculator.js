743159107eff56beb26d9c5cfdb5250425261234
veena-02 <vc7373@srmist.edu.in>
2020-12-25 11:31:27 +0530

    adding bmi calculator feature

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,12 +8,26 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 app.post("/",function(req,res){
-    const x=Number(req.body.num1);
-    const y=Number(req.body.num2);
-    const result=x+y;
+    let x=Number(req.body.num1);
+    let y=Number(req.body.num2);
+    let result=x+y;
     res.send("The sum is "+result);
 });
-
+app.get('/bmiCalculator',function(req,res){
+    res.sendFile(__dirname+"/bmicalculator.html");
+});
+app.post('/bmicalculator',function(req,res){
+    let height=Number(req.body.ht);
+    let weight=Number(req.body.wt);
+    let bmi=weight/(height*height);
+    bmi=Math.round(bmi*10000);
+    let bmiCategory;
+    if(bmi<18.5)bmiCategory="Underweight";
+    else if(bmi>=18.5 && bmi<25)bmiCategory="Normal weight";
+    else if(bmi>=25 && bmi<30)bmiCategory="Overweight";
+    else bmiCategory="Obese";
+    res.send("<h3>Your Body Mass Index is<h3>"+bmi +"<p>You are</p>"+bmiCategory); 
+})
 app.listen(3000,()=>{
     console.log("Server starts at port 3000");
 });
